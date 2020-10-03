9f6948a2f996ccc13c4bc2bd04f77e5e5496c025
Mac <mac@macs-imac-9.local>
2020-10-03 01:36:09 -0700

    ddfdfd

--- sss/ViewController.swift
+++ sss/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var a  = 10;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
